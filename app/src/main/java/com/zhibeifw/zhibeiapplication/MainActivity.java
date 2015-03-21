0f7c841a03e4d799b8e2cc96ba8c41fe8dfd0e1b
fjjohnchen <fjjohnchen@qq.com>
2015-03-22 00:19:44 +0800

    添加图标

--- app/src/main/java/com/zhibeifw/zhibeiapplication/MainActivity.java
+++ app/src/main/java/com/zhibeifw/zhibeiapplication/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends MaterialNavigationDrawer {
 
     @Override
     public void init(Bundle savedInstanceState) {
-        this.addSection(newSection(getString(R.string.puja), new Fragment()));
+        this.addSection(newSection(getString(R.string.puja),R.drawable.ic_puja,new Fragment()));
 
     }
 
