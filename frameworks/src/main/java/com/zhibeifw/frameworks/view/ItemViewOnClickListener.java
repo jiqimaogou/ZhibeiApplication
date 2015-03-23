5f146c2d66c2f84a6dfd283b74938abe0de4b364
fjjohnchen <fjjohnchen@qq.com>
2015-03-23 08:46:29 +0800

    添加ImageAdapter和ItemViewClickListener
    ItemViewClickListener主要用于避免多次创建ClickLIstener.

---
+++ frameworks/src/main/java/com/zhibeifw/frameworks/view/ItemViewOnClickListener.java
@@ -0,0 +1,66 @@
+package com.zhibeifw.frameworks.view;
+
+import android.view.View;
+
+/**
+ * Created by Administrator on 2015/3/23 0023.
+ */
+public class ItemViewOnClickListener implements View.OnClickListener {
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        final ItemInfoHolder itemInfoHolder = (ItemInfoHolder) v.getTag();
+        if (itemInfoHolder == null) {
+            return;
+        }
+        Object[] objs = itemInfoHolder.objs;
+
+        if (objs == null || objs.length == 0) {
+            onClick(v, itemInfoHolder.position);   
+        } else if (objs.length == 1) {
+            onClick(v, itemInfoHolder.position, objs[0]);
+        } else if (objs.length == 2) {
+            onClick(v, itemInfoHolder.position, objs[0]);
+        } else {
+            onClick(v, itemInfoHolder.position, objs[0], objs[1]);
+        }
+    }
+
+    public void onClick(View v, int position) {
+        
+    }
+
+    public void onClick(View v, int position, Object object) {
+        
+    }
+
+    public void onClick(View v, int position, Object object1, Object object2) {
+        
+    }
+
+    /**
+     * Created by Administrator on 2015/3/23 0023.
+     */
+    public static class ItemInfoHolder {
+
+        public int position;
+
+        public Object[] objs;
+
+        public ItemInfoHolder(int position, Object... objs) {
+            this.position = position;
+            this.objs = objs;
+        }
+
+        public static ItemInfoHolder attach(View v, int position, Object... objs) {
+            ItemInfoHolder itemInfoHolder = new ItemInfoHolder(position, objs);
+            v.setTag(itemInfoHolder);
+            return itemInfoHolder;
+        }
+    }
+}
